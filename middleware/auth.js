9eb3c551e88d8dcd21562ae629f781fd7d5f8d18
Ahmad 04-88 <ahmadalmohamad.swi@gmail.com>
2025-12-04 02:51:33 +0300

    fix auth default export

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+export default function auth(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "غير مصرح" });
+  }
+
+  // لاحقاً سنضيف JWT حقيقي
+  next();
+}
